39056216147fbc48a8ef1daeb44b7b720ef7c0cb
Neal A <73256760+its-notneal1@users.noreply.github.com>
2021-03-27 02:25:55 -0700

    Add files via upload

---
+++ Membership.cpp
@@ -0,0 +1,8 @@
+// client for membership client
+// used to make changes to the members
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include "Member.h" //  
+#include "ClientLib.h" //
